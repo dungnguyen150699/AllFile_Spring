91838c1b99295fb24028eed19ad5f2f580e94de6
tiendung <dungnguyen150699@gmail.com>
2021-12-24 10:23:12 +0700

    update

--- sementer2021I/src/main/java/xulyform/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package xulyform.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import xulyform.entities.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long>  {
-	
-}
